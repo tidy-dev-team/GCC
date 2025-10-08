ffc47d9278c08572108b7024b4fcf932df729172
Adir <slutzkis@gmail.com>
2025-10-08 15:07:43 +0300

    updates

--- Template_03/carousel.js
+++ Template_03/carousel.js
@@ -1,7 +1,28 @@
 function caroPrev(e) {
-    e.target.parentNode.parentNode.getElementsByClassName('carousel')[0].scrollBy(-322, 0)
+    window.caro = e.target.parentNode.parentNode.getElementsByClassName('carousel')[0];
+    window.caro.scrollBy(-322, 0);
 }
 
 function caroNext(e) {
-    e.target.parentNode.parentNode.getElementsByClassName('carousel')[0].scrollBy(322, 0)
+    window.caro = e.target.parentNode.parentNode.getElementsByClassName('carousel')[0];
+    window.caro.scrollBy(322, 0);
+}
+
+function caroCheck(e) {
+    if (window.caro == undefined) {
+        window.caro = e.target;
+    }
+
+    window.caro.parentNode.getElementsByClassName('btn-icon')[0].classList.remove('disabled');
+    window.caro.parentNode.getElementsByClassName('btn-icon')[1].classList.remove('disabled');
+
+    setInterval(function () {
+        if (window.caro.scrollLeft == 0) {
+            window.caro.parentNode.getElementsByClassName('btn-icon')[0].classList.add('disabled');
+        }
+
+        if (window.caro.scrollLeft + window.caro.clientWidth >= window.caro.scrollWidth) {
+            window.caro.parentNode.getElementsByClassName('btn-icon')[1].classList.add('disabled');
+        }
+    }, 10);
 }
