eb0b25b0d0bcf7d121af5095314bf69a580383f6
Adir <slutzkis@gmail.com>
2025-10-08 14:53:59 +0300

    updates

--- Template_03/carousel.js
+++ Template_03/carousel.js
@@ -1,3 +1,7 @@
 function caroPrev(e) {
-    alert(e.target);
+    e.target.parentNode.parentNode.getElementsByClassName('carousel')[0].scrollBy(-322, 0)
+}
+
+function caroNext(e) {
+    e.target.parentNode.parentNode.getElementsByClassName('carousel')[0].scrollBy(322, 0)
 }
